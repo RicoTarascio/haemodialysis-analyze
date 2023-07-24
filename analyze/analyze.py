7555c3ffad668255e6769c6331e52c72669ab68c
Federico Tarascio <fe.tarascio@gmail.com>
2023-07-24 12:30:31 +0200

    Analyze init

--- analyze/analyze.py
+++ analyze/analyze.py
@@ -1,33 +1,93 @@
 import pandas as pd
-import os
+import os, sys
 
 ROOT_DIR = os.path.abspath(os.curdir)
 DEFAULT_ORIG_FOLDER_PATH = os.path.join(ROOT_DIR, "original")
 class Analyze:
     original_folder_path: str
     patients_files: list[os.DirEntry]
+    parameters = []
     
-    def __init__(self, original_folder_path: str) -> None:
+    def __init__(self, original_folder_path: str, parameters_file_path: str) -> None:
         if not os.path.exists(original_folder_path):
             print(
-                "[WARNING] Original files folder was not found, one will be created for you in ./original."
+                "[WARNING]: Original files folder was not found, one will be created for you in ./original."
             )
             os.makedirs(DEFAULT_ORIG_FOLDER_PATH, exist_ok=True)
+
+        
+        if not os.path.exists(parameters_file_path):
+            sys.exit("[ERROR]: Parameters file was not found in path: " + parameters_file_path)
+        
+        if not Analyze.is_file_valid(parameters_file_path):
+            sys.exit("[ERROR]: Parameters file is not an excel file (.xlsx or .xls).")
+
+        self.parameters = Analyze.read_parameters(parameters_file_path)
 
         original_dir = Analyze.get_dir(original_folder_path)
         valid_files = list(filter(Analyze.is_file_valid, original_dir))
 
-        print(valid_files)
-
         self.original_folder_path = original_folder_path
         self.patients_files = valid_files
+
+        print(self.analyze_patient(self.patients_files[0]))
+
+    @staticmethod
+    def read_patient_data(patient_file: os.DirEntry, sheet_name: str | None):
+        df = pd.read_excel(patient_file, sheet_name=sheet_name)
+        return df.to_numpy().tolist()
+    
+    @staticmethod
+    def get_month_dates(file: pd.ExcelFile, month):
+        dates = {}
+        pre = 0
+        for x in Analyze.read_patient_data(file, month)[1]:
+            x = str(x)
+            if x.find("-") > -1:
+                if int(x.split("-")[0]) > pre:
+                    dates[x.split("-")[0]] = 0
+                    pre = int(x.split("-")[0])
+                else: break
+        return dates
+    
+    def analyze_patient(self, patient_file: os.DirEntry):
+        
+
+        months = {}
+
+        f = pd.ExcelFile(patient_file)
+
+        for month in f.sheet_names:
+            if month not in ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"]:
+                continue
+            dates = Analyze.get_month_dates(f, month)
+
+            
+            
+            
+        return {m: self.parameters for m in months}
+
+    @staticmethod
+    def read_parameters(parameters_file_path: str): 
+        df = pd.read_excel(parameters_file_path)
+        full_read = df.to_numpy()
+        parameters = [x.tolist() for x in full_read if not pd.isna(x[0]) and not pd.isna(x[1]) and not pd.isna(x[2])]
+
+        return parameters
 
     @staticmethod
     def get_dir(path: str):
         return list(os.scandir(path))
     
     @staticmethod
-    def is_file_valid(file: os.DirEntry):
-        if str(file.name).find(".xlxs") == -1 and str(file.name).find(".xls") == -1:
+    def is_file_valid(file: os.DirEntry | str):
+        if isinstance(file, os.DirEntry):
+            if str(file.name).find(".xlxs") == -1 and str(file.name).find(".xls") == -1:
+                return False
+            return True
+        if file.find(".xlxs") == -1 and file.find(".xls") == -1:
             return False
         return True
+    
+
+
--- analyze/tests/__init__.py
+++ analyze/tests/__init__.py
@@ -3,5 +3,6 @@ import os
 
 ROOT_DIR = os.path.abspath(os.curdir)
 ORIGINAL_FOLDER_PATH = os.path.join(ROOT_DIR, "analyze/tests/original")
+PARAMETERS_PATH = os.path.join(ROOT_DIR, "analyze/tests/ranges.xlsx")
 
-anal = Analyze(ORIGINAL_FOLDER_PATH)
+anal = Analyze(ORIGINAL_FOLDER_PATH, PARAMETERS_PATH)
